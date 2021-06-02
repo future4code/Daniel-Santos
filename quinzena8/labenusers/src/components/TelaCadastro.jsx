209ef94ca4134568316e2c9c66ac376669e585a0
unknown <dandan_187@hotmail.com>
2021-06-01 23:00:42 -0300

    exercício-quinzena8-aula27

--- quinzena8/labenusers/src/components/TelaCadastro.jsx
+++ quinzena8/labenusers/src/components/TelaCadastro.jsx
@@ -1,12 +1,57 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class Cadastro extends React.Component {
+    state = {
+        nome:"",
+        email:"",
+    }
+
+    hadleNome = (event) => {
+        this.setState({nome: event.target.value})
+    }
+
+    handleEmail = () => {
+        this.setState({email: event.target.value})
+    }
+
+    efetuarCadastro = () => {
+        const url ='https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users'
+        const body = {
+            name: this.state.nome,
+            email: this.state.email
+        } 
+
+        axios.post(url, body, {
+            headers: {
+                Authorization:"Daniel-Santos"
+            }
+        })
+        .then((res) => {
+            console.log()
+        })
+        .catch((err) => {
+            console.log()
+        })
+    }
+
     render()
     {
         return(
             <div>
                 <button onClick={this.props.irLista}>Ir para lista de usuários</button>
                 <h2>Cadastro</h2>
+                <input 
+                    placeholder={"nome"}
+                    value={this.state.nome}
+                    onChange={this.hadleNome()}
+                />
+                <input 
+                    placeholder={"e-mail"}
+                    value={this.state.email}
+                    onChange={this.handleEmail()}
+                />
+                <button onClick={this.efetuarCadastro}/>
             </div>
         )
     }
--- quinzena8/labenusers/src/components/TelasLista.jsx
+++ quinzena8/labenusers/src/components/TelasLista.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 export default class Lista extends React.Component {
     render(){
         return(
-            <div>Lista</div>
+            <div>
+                <butto onClick={this.props.irCadastro}>Ir para Cadastro</butto>
+                <h2>
+                    Lista de usuários
+                </h2>
+            </div>
         )
     }
 }
