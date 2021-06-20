f8dc355e61baf6723dfaa0d91808f960f850341c
unknown <dandan_187@hotmail.com>
2021-06-19 21:11:37 -0300

    commit1

--- projeto8/astromatch/src/components/CardUsuário/CardUsuario.jsx
+++ projeto8/astromatch/src/components/CardUsuário/CardUsuario.jsx
@@ -5,26 +5,65 @@ export default function CardPerfilUsuario (props) {
 
     const [perfil, setPerfil] = useState([])
 
-    const pegaPerfil = () => {
+    
+
+    const chamarPerfil = () => {
         const url = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:aluno/person"
         axios
         .get (url)
         .then((response => {
-        console.log(response.data);
-        setPerfil(response.data)
+            console.log(response.data.profile);
+            setPerfil(response.data.profile)
+        })
+        .catch (err => {
+            console.log(err.response, "er!!!")
         })
         )
     }
 
+
+    const escolherPerfil = (id, choice) => {
+        const url = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:aluno/choose-person"
+        axios.post(url, {id, choice})
+        .then(chamarPerfil())
+        .catch(err => {
+            console.log( err, "erro!!!")
+        })
+        
+    }
+
+    const limpar = () => {
+        const url = "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/:aluno/clear"
+        axios.get (url)
+        .then(chamarPerfil())
+        .catch(err => {
+            console.log(err,"err!!!")
+        })
+    }
+
+
     useEffect(() => {
-        pegaPerfil();
+        chamaPerfil();
       }, [])
+
+
+    const infoBio = ( ) => {
+
+        return (
+            <div className="CardFoto">
+                <img className="photo" src={perfil.photo}></img>
+                <div className="name">{perfil.name}, {perfil.age} anos</div>
+                <div className="Bio">{perfil.bio}</div>
+            </div>
+        )
+        
+    }
 
     
 
     return (
         <div>
-            {perfil.name}
+            {infoBio()}
         </div>
     )
 }
