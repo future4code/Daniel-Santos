bd3c832458e72fb7a67f3b6eb81b640cf6bafcf2
unknown <dandan_187@hotmail.com>
2021-08-03 22:15:34 -0300

    quin11-aula40

--- quinzena11/aula40/index.js
+++ quinzena11/aula40/index.js
@@ -1,67 +1,83 @@
-const findFirstRecurringCharacter = (input) => {
-    const charHashMap = {};
-    for (const char of input) {
-      if (charHashMap[char] === true) {
-        return char;
-      }
-      charHashMap[char] = true;
-    }
-    return null;
-  };
+// const findFirstRecurringCharacter = (input) => {
+//     const charHashMap = {};
+//     for (const char of input) {
+//       if (charHashMap[char] === true) {
+//         return char;
+//       }
+//       charHashMap[char] = true;
+//     }
+//     return null;
+//   };
 
-  // 1. O(n)
+//   // 1. O(n)
 
-  export const func = (
-    source,
-    comparison
-  ) => {
-    if (
-      comparison.length > source.length + 1 ||
-      comparison.length < source.length - 1
-    ) {
-      return false;
-    }
-    let commonCharQuantity = 0;
+//   export const func = (
+//     source,
+//     comparison
+//   ) => {
+//     if (
+//       comparison.length > source.length + 1 ||
+//       comparison.length < source.length - 1
+//     ) {
+//       return false;
+//     }
+//     let commonCharQuantity = 0;
   
-    for (const char of comparison) {
-      if (source !== comparison) {
-        commonCharQuantity++;
-      }
-    }
-    return (
-      commonCharQuantity <= source.length + 1 &&
-      commonCharQuantity >= source.length - 1
-    );
-  };
+//     for (const char of comparison) {
+//       if (source !== comparison) {
+//         commonCharQuantity++;
+//       }
+//     }
+//     return (
+//       commonCharQuantity <= source.length + 1 &&
+//       commonCharQuantity >= source.length - 1
+//     );
+//   };
 
-  // 2. O(n)
+//   // 2. O(n)
 
-  export const replaceMatrixValue = (
-    matrix,
-    rowIndex,
-    columnIndex,
-    value
-  ) => {
-    if (
-      matrix[rowIndex] === undefined ||
-      matrix[rowIndex][columnIndex] === undefined
-    ) {
-      throw new Error("Fora do intervalo da matriz");
-    }
+//   export const replaceMatrixValue = (
+//     matrix,
+//     rowIndex,
+//     columnIndex,
+//     value
+//   ) => {
+//     if (
+//       matrix[rowIndex] === undefined ||
+//       matrix[rowIndex][columnIndex] === undefined
+//     ) {
+//       throw new Error("Fora do intervalo da matriz");
+//     }
   
-    matrix[rowIndex][columnIndex] = value;
-  };
+//     matrix[rowIndex][columnIndex] = value;
+//   };
 
 
-  //3. O(n)
+//   //3. O(n)
 
-  function verifyIfExistRepeatedNumbers(listOfNumbers) {
-    for (let i = 0; i < listOfNumbers.length; i++) {
-      if (listOfNumbers.indexOf(listOfNumbers[i]) !== i) { 
-        return true;
-      }
-    }
-    return false;
+//   function verifyIfExistRepeatedNumbers(listOfNumbers) {
+//     for (let i = 0; i < listOfNumbers.length; i++) {
+//       if (listOfNumbers.indexOf(listOfNumbers[i]) !== i) { 
+//         return true;
+//       }
+//     }
+//     return false;
+//   }
+
+//   //4. O(n²) => Aqui index of é uma função de callback que em sua estrutura tem um loop que percorre todos os indices do Array
+
+const getArrayInformation = (array) => {
+  let sum = 0;
+  let product = 1;
+  
+  for (const num of array) {
+  sum += num;
+  product *= num;
   }
-
-  //4. O(n²) => Aqui index of é uma função de callback que em sua estrutura tem um loop que percorre todos os indices do Array
+  
+  return {
+  quantity: array.length,
+  sum,
+  product,
+  };
+  };
